6f72fc8fc3616d9ad1c2ec86d8123cb2dff8d49f
David Sherlock <davidsherlock163@hotmail.com>
2016-03-03 09:38:41 +0000

    fixed swiftcsv import,

--- RSTriva/ViewController.swift
+++ RSTriva/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftCSV
 
 class ViewController: UIViewController {
 
@@ -16,6 +17,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var Button3: UIButton!
     @IBOutlet weak var Button4: UIButton!
     
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -26,11 +29,56 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func RandQuestions(){
+       
+        
+        let questions = readCSV("questions.csv")
+        
+        var RandomNumber = arc4random() % 100
+        RandomNumber += 1
+        
+        print(questions.rows[1])
+        
+        
+        
+        
+    }
+    
+    
+    func readCSV(URL: String)->CSV{
+        
+        var questions = CSV(string: "id,name,age\n1,Alice,18\n2,Bob,19")
+        
+        do {
+            let questions = try CSV(name: "questions.csv")
+        } catch {
+            //error handeling
+        }
+        
+        return questions
+ 
+    }
 
     
+    @IBAction func Button1Action(sender: AnyObject) {
+        
+        
+    }
     
     
+    @IBAction func Button2Action(sender: AnyObject) {
+        
+    }
     
+    
+    @IBAction func Button3Action(sender: AnyObject) {
+        
+    }
+    
+    
+    @IBAction func Button4Action(sender: AnyObject) {
+        
+    }
     
 }
 
